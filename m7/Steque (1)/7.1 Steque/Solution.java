bb112baec274d58da6a4e11767cb5f119a5af314
Bharath0852 <bharathkumarpbk1995@gmail.com>
2018-10-11 12:35:38 +0530

    "check_style_score -> 0 of 2 passed. style score: 0/1 "

---
+++ m7/Steque (1)/7.1 Steque/Solution.java
@@ -0,0 +1,122 @@
+package M7;
+import java.util.*;
+
+public class Solution 
+{
+	
+	public static void main (String[] args)
+	{
+		Solution s= new Solution();
+		Scanner sc = new Scanner (System.in);
+		String n1,n2 = null;
+		int num,num1;
+		
+		while(sc.hasNext())
+		{
+			String s1=sc.nextLine();
+			String[] s2=s1.split(" ");
+			n1=s2[0];
+			if(s2.length>1)
+			{
+				n2=s2[1];
+			}
+			//num=Integer.parseInt(n2);
+			switch(n1)
+			{
+				case "push" :
+					s.push(Integer.parseInt(n2));
+					break;
+				case "pop" :
+					s.pop();
+					s.print();
+					break;
+				case "enqueue" :
+					s.enqueue(Integer.parseInt(n2));
+					break;
+			}
+		}
+	}
+
+public class Node 
+{
+	int data;
+	Node next;
+	Node(int d)
+	{
+		data=d;
+		next=null;
+	}
+}
+	Node head;
+	public void push(int a)
+	{
+		Node n= new Node(a);
+		if(head==null)
+		{
+			head=n;
+		}
+		else if(head.next==null)
+		{
+			n.next=head;
+			head=n;
+		}
+		else
+		{
+			n.next=head;
+			head=n;
+		}
+	}
+	public void enqueue(int a)
+	{
+		Node n= new Node(a);
+		if(head==null)
+		{
+			head=n;
+		}
+		else if(head.next==null)
+		{
+			n.next=head;
+			head=n;
+		}
+		else
+		{
+			n.next=head;
+			head=n;
+		}
+	}
+	public int pop()
+	{
+		int temp = 0;
+		if(head==null)
+		{
+			System.out.println("Staque is empty.");
+		}
+		else if(head.next!=null)
+		{
+			temp=head.data;
+			head.next=head;
+			
+		}
+		return temp;
+	}
+	public void print()
+	{
+		Node temp=head;
+		while(temp!=null)
+		{
+			System.out.println(temp.data);
+			temp=temp.next;
+		}
+	}
+
+
+}
+
+
+//node
+
+//operations
+
+
+
+
